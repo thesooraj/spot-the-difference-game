418a89e047708d0e291648501d75f4b8c8c8ae9b
Sooraj Khadka <suryakirankhadka@gmail.com>
2026-05-12 22:29:20 +1000

    Initial project setup and imports

---
+++ spot_the_difference.py
@@ -0,0 +1,12 @@
+"""
+HIT137 - Group Assignment 3
+Spot the Difference Game
+Authors: Sooraj, Fariha, Daniyal, Hassan
+"""
+
+import cv2
+import numpy as np
+import random
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
